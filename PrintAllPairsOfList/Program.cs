1e986cbada03d97331f896822391380578b1b69c
Gowrish <gowrishkumar@live.com>
2022-06-25 20:33:17 -0400

    example of O(n^2)

---
+++ PrintAllPairsOfList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrintAllPairsOfList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int []{1, 2, 3, 4, 5};
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    Console.WriteLine($"{input[i]}, {input[j]}");
+                }
+            }
+        }
+    }
+}
